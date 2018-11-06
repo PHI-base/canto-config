d94f1566874cb94e60094deb564dd114681369e4
James Seager <james.seager@rothamsted.ac.uk>
2018-11-06 13:01:41 +0000

    Remove FYPO from the ontology list
    This ontology was only included temporarily in order to keep phenotype 
    annotations working; it's been superseded by PHIPO now.

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -5,7 +5,6 @@ cd /var/canto_space/
 ./canto/script/canto_docker ./script/canto_load.pl \
 --process-extension-config \
 --ontology http://snapshot.geneontology.org/ontology/go-basic.obo \
---ontology https://curation.pombase.org/ontologies/fypo/latest/fypo-simple.obo \
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
 --ontology https://raw.githubusercontent.com/pombase/fypo/master/peco.obo \
 --ontology https://raw.githubusercontent.com/PHI-base/phipo/b00c3cbd463c956b159ce8967cc23b8cdf873dd3/phipo.obo \
