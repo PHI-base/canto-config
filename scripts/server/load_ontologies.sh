005bed5ca60cf07dc867541be0c3efc1a88aa172
James Seager <james.seager@rothamsted.ac.uk>
2018-11-13 11:08:16 +0000

    Add PomBase relations ontology to loading script
    Note that the native loading script has a different path

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -7,6 +7,7 @@ cd /var/canto_space/
 --ontology http://snapshot.geneontology.org/ontology/go-basic.obo \
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
 --ontology https://raw.githubusercontent.com/pombase/fypo/master/peco.obo \
+--ontology /canto/etc/ro.obo \
 --ontology https://raw.githubusercontent.com/PHI-base/phipo/b00c3cbd463c956b159ce8967cc23b8cdf873dd3/phipo.obo \
 --ontology http://data.bioontology.org/ontologies/BTO/submissions/33/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb \
 --ontology /import_export/phipo_extension_relations.obo
