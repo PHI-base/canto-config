486664e1d06d163f0bf1f6002a16e72d716df5a8
James Seager <james.seager@rothamsted.ac.uk>
2018-11-15 13:35:05 +0000

    Update PHIPO version

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -8,6 +8,6 @@ cd /var/canto_space/
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
 --ontology /import_export/phi-eco.obo \
 --ontology /canto/etc/ro.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phipo/b00c3cbd463c956b159ce8967cc23b8cdf873dd3/phipo.obo \
+--ontology https://raw.githubusercontent.com/PHI-base/phipo/a87351db4b07c4b21906cab1ef460148cf5889bc/phipo.obo \
 --ontology http://data.bioontology.org/ontologies/BTO/submissions/33/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb \
 --ontology /import_export/phipo_extension_relations.obo
