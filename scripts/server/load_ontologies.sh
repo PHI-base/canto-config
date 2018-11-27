baf2cd2b12740d4de833227e4e55bbaf186809a7
James Seager <james.seager@rothamsted.ac.uk>
2018-11-27 13:17:26 +0000

    Disable extension processing in ontology script
    This should ensure that the ontology loading doesn't fail.

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -3,11 +3,8 @@
 cd /var/canto_space/
 
 ./canto/script/canto_docker ./script/canto_load.pl \
---process-extension-config \
 --ontology http://snapshot.geneontology.org/ontology/go-basic.obo \
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
 --ontology /import_export/phi-eco.obo \
 --ontology /canto/etc/ro.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phipo/a87351db4b07c4b21906cab1ef460148cf5889bc/phipo.obo \
---ontology http://data.bioontology.org/ontologies/BTO/submissions/33/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb \
---ontology /import_export/phipo_extension_relations.obo
+--ontology https://raw.githubusercontent.com/PHI-base/phipo/a87351db4b07c4b21906cab1ef460148cf5889bc/phipo.obo
