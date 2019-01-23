98cc7f967916ba0d9e1facc095a6b0fe55daaa38
James Seager <james.seager@rothamsted.ac.uk>
2019-01-23 11:56:42 +0000

    Pin PHIPO to the latest version (master)

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -8,7 +8,7 @@ cd /var/canto_space/
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
 --ontology /import_export/phi-eco.obo \
 --ontology /canto/etc/ro.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phipo/a87351db4b07c4b21906cab1ef460148cf5889bc/phipo.obo \
+--ontology https://raw.githubusercontent.com/PHI-base/phipo/master/phipo.obo \
 --ontology /import_export/BrendaTissue.obo \
 --ontology /import_export/phipo_extension_relations.obo \
 --ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/has_qualifier_range.obo
