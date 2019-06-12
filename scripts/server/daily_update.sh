17a0a87e705c41415ac94d4280abf66422d40359
James Seager <james.seager@rothamsted.ac.uk>
2019-06-12 14:37:23 +0100

    Remove organism reloading from daily_update.sh
    The server isn't currently able to sync itself to the config repository where the organism data is stored, so it would make more sense to run this loading on demand instead.

--- scripts/server/daily_update.sh
+++ scripts/server/daily_update.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-cd /var/canto_space/ || exit 1
-
-cp config_canto/data/host_species.csv canto/host_species.csv
-cp config_canto/data/host_strains.csv canto/host_strains.csv
-cp config_canto/data/pathogen_species.csv canto/pathogen_species.csv
-cp config_canto/data/pathogen_strains.csv canto/pathogen_strains.csv
-
-./canto/script/canto_docker ./script/canto_load.pl --organisms host_species.csv
-./canto/script/canto_docker ./script/canto_load.pl --strains host_strains.csv
-./canto/script/canto_docker ./script/canto_load.pl --organisms pathogen_species.csv
-./canto/script/canto_docker ./script/canto_load.pl --strains pathogen_strains.csv
-
-rm canto/host_species.csv
-rm canto/host_strains.csv
-rm canto/pathogen_species.csv
-rm canto/pathogen_strains.csv
-
 /var/canto_space/config_canto/scripts/server/load_ontologies.sh
