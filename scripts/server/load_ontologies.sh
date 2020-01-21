f45a0b93da18c853a2bf22dfb8f63accae2f8fd3
James Seager <james.seager@rothamsted.ac.uk>
2020-01-21 13:00:35 +0000

    Load PHI-ECO and PHIDO from GitHub

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -6,7 +6,7 @@ cd /var/canto_space/
 --process-extension-config \
 --ontology http://snapshot.geneontology.org/ontology/go-basic.obo \
 --ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
---ontology /import_export/phi-eco.obo \
+--ontology https://raw.githubusercontent.com/PHI-base/phi-eco/master/phi-eco.obo \
 --ontology /canto/etc/ro.obo \
 --ontology http://purl.obolibrary.org/obo/phipo/phipo-simple.obo \
 --ontology http://purl.obolibrary.org/obo/bto.obo \
@@ -14,4 +14,4 @@ cd /var/canto_space/
 --ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/has_qualifier_range.obo \
 --ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/fypo_extension.obo \
 --ontology /import_export/phipo_namespace_destroyer.obo \
---ontology /import_export/phido.obo
+--ontology https://raw.githubusercontent.com/PHI-base/phido/master/phido.obo
