0a7a8409d68a8b15d3f9bdf2fe83d78206349690
James Seager <james.seager@rothamsted.ac.uk>
2020-02-04 14:08:58 +0000

    Add script to update GO extension config

---
+++ scripts/server/update_go_config.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+github_url="https://raw.githubusercontent.com/pombase/pombase-config/master/canto/annotation_ex_config"
+destination="/var/canto_space/import_export"
+
+download() {
+    url=$1;
+    path=$2;
+    headers="$(wget -O "$path" -q --server-response "$url" 2>&1)"
+    if ! (echo "$headers" | head -n 1 | grep -q "200"); then
+      echo "ERROR: did not get status code 200 for URL:\n${url}" 1>&2
+      exit 1;
+    fi
+}
+
+download "${github_url}/GO_BP_A_E_config" /tmp/GO_BP_A_E_config.tsv
+download "${github_url}/GO_CC_A_E_config" /tmp/GO_CC_A_E_config.tsv
+download "${github_url}/GO_MF_A_E_config" /tmp/GO_MF_A_E_config.tsv
+
+exit 1;
+
+mv /tmp/GO_BP_A_E_config.tsv ${destination}
+mv /tmp/GO_CC_A_E_config.tsv ${destination}
+mv /tmp/GO_MF_A_E_config.tsv ${destination}
