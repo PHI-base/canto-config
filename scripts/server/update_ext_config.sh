67f7a955fe7e039ea63ac9ff51a5a2e22a7c1065
James Seager <james.seager@rothamsted.ac.uk>
2023-06-29 15:22:42 +0100

    Update paths in update_ext_config.sh

--- scripts/server/update_ext_config.sh
+++ scripts/server/update_ext_config.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-github_url="https://raw.githubusercontent.com/pombase/pombase-config/master/canto/annotation_ex_config"
-destination="/var/canto_space/import_export"
+# Change to directory of the current script file
+cd "$(dirname "$(readlink -f -- "$0")")" || exit
 
-if [ ! -d "${destination}" ]; then
+github_url="https://raw.githubusercontent.com/pombase/pombase-config/master/canto/annotation_ex_config"
+destination="./import_export"
+
+if [ ! -d "$destination" ]; then
   echo "ERROR: directory does not exist: ${destination}" 1>&2
   exit 1;
 fi
@@ -18,14 +21,16 @@ download() {
     fi
 }
 
-download "${github_url}/GO_BP_A_E_config" /tmp/GO_BP_A_E_config.tsv
-download "${github_url}/GO_CC_A_E_config" /tmp/GO_CC_A_E_config.tsv
-download "${github_url}/GO_MF_A_E_config" /tmp/GO_MF_A_E_config.tsv
-download "${github_url}/PSI-MOD_A_E_config" /tmp/PSI-MOD_A_E_config.tsv
-download "${github_url}/PomGeneEx_A_E_config" PomGeneEx_A_E_config.tsv
+cd $destination
 
-mv /tmp/GO_BP_A_E_config.tsv ${destination}
-mv /tmp/GO_CC_A_E_config.tsv ${destination}
-mv /tmp/GO_MF_A_E_config.tsv ${destination}
-mv /tmp/PSI-MOD_A_E_config.tsv ${destination}
-mv /tmp/PomGeneEx_A_E_config.tsv" ${destination}
+download "$github_url/GO_BP_A_E_config" GO_BP_A_E_config.tmp.tsv
+download "$github_url/GO_CC_A_E_config" GO_CC_A_E_config.tmp.tsv
+download "$github_url/GO_MF_A_E_config" GO_MF_A_E_config.tmp.tsv
+download "$github_url/PSI-MOD_A_E_config" PSI-MOD_A_E_config.tmp.tsv
+download "$github_url/PomGeneEx_A_E_config" PomGeneEx_A_E_config.tmp.tsv
+
+mv GO_BP_A_E_config.tmp.tsv GO_BP_A_E_config.tsv
+mv GO_CC_A_E_config.tmp.tsv GO_CC_A_E_config.tsv
+mv GO_MF_A_E_config.tmp.tsv GO_MF_A_E_config.tsv
+mv PSI-MOD_A_E_config.tmp.tsv PSI-MOD_A_E_config.tsv
+mv PomGeneEx_A_E_config.tmp.tsv PomGeneEx_A_E_config.tsv
