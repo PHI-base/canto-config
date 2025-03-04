6b7326f20b26fce81a7ec653d8340a652d9e1276
James Seager <james.seager@rothamsted.ac.uk>
2025-03-04 13:19:48 +0000

    Enable encryption of curation session backups

--- scripts/server/backup_curation_sessions.sh
+++ scripts/server/backup_curation_sessions.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 backup_dir=$1
+encrypted_dir=$2
 
 # Change directory to the location of the script file:
 # The script file should be in the canto_space directory
 canto_dir="$(dirname "$(readlink -f -- "$0")")"
 
-# Default to canto_space/backup/ if no backup directory is specified
+# Default directories for backup and encrypted files
 backup_dir=${backup_dir:="$canto_dir/backup"}
+encrypted_dir=${encrypted_dir:="$backup_dir/encrypted"}
 
 # Temporary directory for making sqlite backups
 sql_export_dir="$canto_dir/import_export/sql_backup"
@@ -18,7 +20,10 @@ sqlite3_cmd="$canto_dir/canto/script/canto_docker --non-interactive sqlite3"
 # Use ISO 8601 basic format for dates, with UTC+0 timezone
 date_str=$(date --utc "+%Y%m%dT%H%M%SZ")
 
-backup_path="$backup_dir/canto_backup_$date_str.tar.gz"
+backup_name="canto_backup_$date_str.tar.gz"
+backup_path="$backup_dir/$backup_name"
+encrypted_path="$encrypted_dir/$backup_name.gpg"
+recipients_path="$canto_dir/recipients.txt"
 
 if [ ! -d "$backup_dir" ]; then
   echo "backup directory not found at $backup_dir"
@@ -49,7 +54,26 @@ for i in *.sqlite3; do
   $sqlite3_cmd "/data/$i" ".backup '/import_export/sql_backup/$i'");
 done
 
+cd "$canto_dir" || exit
+
 tar -cf - -C "$sql_export_dir" . | gzip -9 > "$backup_path"
+
+# Create encrypted backups if the email addresses of any recipients
+# are specified in recipients.txt
+if [ -f "$recipients_path" ]; then
+  if [ ! -d "$encrypted_dir" ]; then
+    mkdir "$encrypted_dir"
+  fi
+
+  recipients=""
+
+  while IFS= read -r email; do
+      recipients="$recipients --recipient $email"
+  done < "$recipients_path"
+  
+  gpg --encrypt --output "$encrypted_path" $recipients "$backup_path"
+  rm "$backup_path"
+fi
 
 # Remove temporary backup files
 rm "$sql_export_dir"/*
