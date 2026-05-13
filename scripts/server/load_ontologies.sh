8632cdae107a106231aff57c2e68ce81ede57132
James Seager <james.seager@rothamsted.ac.uk>
2026-05-13 12:08:26 +0100

    Change load_ontologies.sh to use local paths

--- scripts/server/load_ontologies.sh
+++ scripts/server/load_ontologies.sh
@@ -4,17 +4,17 @@ cd /var/canto_space/
 
 ./canto/script/canto_docker --non-interactive ./script/canto_load.pl \
 --process-extension-config \
---ontology http://snapshot.geneontology.org/ontology/go-basic.obo \
---ontology https://curation.pombase.org/ontologies/PSI-MOD-2016-01-19.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phi-eco/master/phi-eco.obo \
+--ontology /import_export/ontologies/go-basic.obo \
+--ontology /import_export/ontologies/PSI-MOD-2016-01-19.obo \
+--ontology /import_export/ontologies/phi-eco.obo \
 --ontology /canto/etc/ro.obo \
---ontology http://purl.obolibrary.org/obo/phipo/phipo-simple.obo \
+--ontology /import_export/ontologies/phipo-simple.obo \
 --ontology /import_export/bto_isa.obo \
 --ontology /import_export/phipo_extension_relations.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phipo_ext/master/phipo_ext.obo \
---ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/has_qualifier_range.obo \
---ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/fypo_extension.obo \
---ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/pombase_gene_expression_ontology.obo \
---ontology https://curation.pombase.org/dumps/latest_build/pombe-embl/mini-ontologies/chebi.obo \
---ontology https://raw.githubusercontent.com/PHI-base/phido/master/phido.obo \
---ontology http://purl.obolibrary.org/obo/so/so-simple.obo
+--ontology /import_export/ontologies/phipo_ext.obo \
+--ontology /import_export/ontologies/has_qualifier_range.obo \
+--ontology /import_export/ontologies/fypo_extension.obo \
+--ontology /import_export/ontologies/pombase_gene_expression_ontology.obo \
+--ontology /import_export/ontologies/chebi.obo \
+--ontology /import_export/ontologies/phido.obo \
+--ontology /import_export/ontologies/so-simple.obo
