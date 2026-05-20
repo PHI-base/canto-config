a18d1bd9e63bcac3e3bd18581c9c76686aa2b607
James Seager <james.seager@rothamsted.ac.uk>
2026-05-20 14:32:19 +0100

    Call get_ontologies.sh from daily_update.sh

--- scripts/server/daily_update.sh
+++ scripts/server/daily_update.sh
@@ -3,5 +3,6 @@
 cd /var/canto_space || exit 1
 
 sh update_ext_config.sh
+sh get_ontologies.sh
 sh load_ontologies.sh
 ./canto/script/canto_docker /canto/script/canto_daily.pl https://canto.phi-base.org/
