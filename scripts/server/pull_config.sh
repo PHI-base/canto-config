cab9f62526ab0750a452ca082dc8fe87e9c2d673
James Seager <james.seager@rothamsted.ac.uk>
2026-08-06 12:09:14 +0100

    Remove SSH commands from pull_config.sh
    This isn't needed since the repository is public.

--- scripts/server/pull_config.sh
+++ scripts/server/pull_config.sh
@@ -3,6 +3,4 @@
 # Change to directory of the current script file
 cd "$(dirname "$(readlink -f -- "$0")")" || exit
 
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/phibase_config
 git -C "canto-config" pull
